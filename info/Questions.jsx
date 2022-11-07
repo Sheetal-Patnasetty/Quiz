5fdd068982f2eeebe2a1b29374e4188c9bc8290c
Sheetal Patnasetty <108509711+Sheetal-Patnasetty@users.noreply.github.com>
2022-11-07 10:03:00 +0530

    Add files via upload

---
+++ info/Questions.jsx
@@ -0,0 +1,42 @@
+export const Questions = [
+    {
+      prompt: "Which of the following is used in React.js to increase performane?",
+      optionA: "Virtual DOM",
+      optionB: "Original DOM",
+      optionC: "Both A and B",
+      optionD: "None of the above",
+      asnwer: "optionA",
+    },
+    {
+      prompt: "Which is used to pass data to components from outside?",
+      optionA: "Render with arguments",
+      optionB: "setState",
+      optionC: "PropTypes",
+      optionD: "props",
+      asnwer: "optionD",
+    },
+    {
+      prompt: "What is Babel?",
+      optionA: "Javascript compiler",
+      optionB: "Javascript interpreter",
+      optionC: "Javascript transpiler",
+      optionD: "None of the above",
+      asnwer: "optionA",
+    },
+    {
+      prompt: "In MVC, what does React.js act as?",
+      optionA: "Model",
+      optionB: "Controller",
+      optionC: "Router",
+      optionD: "Middleware",
+      asnwer: "optionB",
+    },
+    {
+      prompt: "Choose the one which helps react for keeping their data uni-directional?",
+      optionA: "DOM",
+      optionB: "Flux",
+      optionC: "Jsx",
+      optionD: "props",
+      asnwer: "optionB",
+    },
+  ];
